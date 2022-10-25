f8c5e426b1787b8958886f7ee87fa3d7813c329a
Markit0h <114784112+Markit0h@users.noreply.github.com>
2022-10-25 13:54:48 -0300

    Primera pagina

---
+++ Nueva carpeta/codigo.js
@@ -0,0 +1,17 @@
+/* string = "cadena de texto" (con comillas)
+number = 12 (sin comillas)
+boolean = false o true
+
+Variable "CONST" (no puede cambiar la variable)
+let y var (se pueden modificar las variables)
+
+var, let, const (trabajan en bloque) */
+
+
+/* Null (Vacio)
+   Undefined (No definida)
+   Nan( No es un numero)
+*/
+
+
+ 
